7b5b17b2faf4002347673e9167abc109ea051fce
Mustapha Babatunde <mustapha.babtunde@andela.co>
2015-06-05 11:23:59 +0100

    Updated hotel model to contain brief description of hotel

--- app/models/hotel.model.js
+++ app/models/hotel.model.js
@@ -17,6 +17,7 @@ var hotelSchema = new Schema({
     rating: { type: String },
     numberOfRooms: { type: Number },
     availableRooms: { type: Number },
+    description: { type: String }
 });
 
 module.exports = mongoose.model('Hotel', hotelSchema);
